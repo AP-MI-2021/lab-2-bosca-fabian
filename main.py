d9d693b437d76e6b2a0afcc19f65aaefaaaf1f95
Bosca Fabian <fabian.bosca@stud.ubbcluj.ro>
2021-10-06 12:16:07 +0300

    Am terminat si a 3-a problema

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 from math import sqrt
 
 
+def is_antipalindrome(n):
+    """
+    Verifica daca numarul este antipalindrom
+    :param n: numarul verificat
+    :return: True daca este antipalindrom, False in caz contrar
+    """
+    if int(n) < 1000:
+        return False
+    lungime_numar = len(n)
+    anti_palindrome = 1
+    for i in range(1, lungime_numar//2):
+        if n[i] == n[lungime_numar-1-i]:
+            anti_palindrome = 0
+        else:
+            anti_palindrome = 1
+
+    return bool(anti_palindrome)
+
+
+def test_is_antipalindrome():
+    assert is_antipalindrome("2773") == 0
+    assert is_antipalindrome("2783") == 1
+    assert is_antipalindrome("12345321") == 1
+
+
 def is_palindrome(n):
     """
     Verifica daca un numar este palindrom
@@ -44,7 +69,7 @@ def is_superprime(n):
     :param n: numarul verificat
     :return: True daca este supeprim, False in caz contrar
     """
-    if n < 0:
+    if n <= 1:
         return False
     while n:
         copie_nr = n
@@ -66,6 +91,7 @@ def main():
     while True:
         print("1. Verificare daca un numar este palindrom")
         print("2. Verificare daca un numar este superprim")
+        print("3. Verificare daca un numar este antipalindrom")
         print("x. Iesire")
         optiune = input("Alege optiunea: ")
         if optiune == '1':
@@ -80,10 +106,17 @@ def main():
                 print(f"Numarul {nr} este superprim")
             else:
                 print(f"Numarul {nr} NU este superprim")
+        elif optiune == '3':
+            nr = input("Introduceti numarul: ")
+            if is_antipalindrome(nr):
+                print(f"Numarul {nr} este antipalindrom")
+            else:
+                print(f"Numarul {nr} NU este antipalindrom")
         elif optiune == 'x':
             break
 
 
 test_is_palindrome()
 test_is_superprime()
+test_is_antipalindrome()
 main()
